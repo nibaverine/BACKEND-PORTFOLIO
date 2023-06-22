d8327094449375ccead5f2e1b86efa9b58a5a6a8
nibaverine <nibaverinekajock@gmail.com>
2023-06-22 03:22:11 -0700

    changes to backend

---
+++ config/app.config.js
@@ -0,0 +1,10 @@
+require("dotenv").config;
+
+const MONGO_DB_CONFIG = {
+   DB: 'mongodb+srv://nibaverine:Verine_20@reactbackendportfolio.gi7xqiw.mongodb.net/ReactBackendProject?retryWrites=true&w=majority'
+    
+}
+
+module.exports = {
+    MONGO_DB_CONFIG
+}
